1d39e84c3e041b00809a7f412d77442e4e12d4ee
jjuny-won <jjuny040627@gamil.com>
2026-09-17 12:16:08 +0900

    0001

---
+++ src/main/java/com/back/jpa/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.back.jpa.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+// 모든 엔티티들의 조상
+public abstract class BaseEntity {
+    public abstract int getId();
+
+    public abstract LocalDateTime getCreateDate();
+
+    public abstract LocalDateTime getModifyDate();
+
+    public String getModelTypeCode() {
+        return this.getClass().getSimpleName();
+    }
+}
